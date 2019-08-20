8494477749f9d1694d144b74527da141133d362b
Jakub Jakubowski <jakubjakubowski@soldevelo.com>
2019-08-20 14:54:36 +0200

    MW4-29 - Fixing problem with checkstyle

--- src/main/java/org/openlmis/integration/dhis2/scheduler/TaskScheduler.java
+++ src/main/java/org/openlmis/integration/dhis2/scheduler/TaskScheduler.java
@@ -12,7 +12,12 @@
  * the GNU Affero General Public License along with this program. If not, see
  * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org.
  */
+
 package org.openlmis.integration.dhis2.scheduler;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TimeZone;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,70 +28,71 @@ import org.springframework.scheduling.config.CronTask;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Service;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TimeZone;
+
+
+
 
 @Service
 public class TaskScheduler implements SchedulingConfigurer {
 
-    private static Logger LOGGER = LoggerFactory.getLogger(TaskScheduler.class);
-    private ScheduledTaskRegistrar newTaskRegistrar;
-    private Set<String> cronExpresions = new HashSet<>();
+  private static Logger LOGGER = LoggerFactory.getLogger(TaskScheduler.class);
+  private ScheduledTaskRegistrar newTaskRegistrar;
+  private Set<String> cronExpresions = new HashSet<>();
 
-    /**
-     * Creates new poolScheduler.
-     */
-    @Bean
-    public org.springframework.scheduling.TaskScheduler poolScheduler() {
-        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
-        scheduler.setThreadNamePrefix("ThreadPoolTaskScheduler");
-        scheduler.setPoolSize(1);
-        scheduler.initialize();
-        return scheduler;
+  /**
+   * Creates new poolScheduler.
+   */
+  @Bean
+  public org.springframework.scheduling.TaskScheduler poolScheduler() {
+    ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+    scheduler.setThreadNamePrefix("ThreadPoolTaskScheduler");
+    scheduler.setPoolSize(1);
+    scheduler.initialize();
+    return scheduler;
+  }
+
+  /**
+  * Creates new task by cron expressions from DB.
+  */
+
+  @Override
+  public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+    newTaskRegistrar = taskRegistrar;
+    newTaskRegistrar.setScheduler(poolScheduler());
+    //cronExpresions.add(schedulerRepository.findAll());
+
+    // this is only for testing, in final version cronExpresions list will come from db
+    cronExpresions.add("* * * * * ?");
+    cronExpresions.add("0/2 * * * * ?");
+    cronExpresions.add("0/3 * * * * ?");
+    cronExpresions.add("0/4 * * * * ?");
+    cronExpresions.add("0/5 * * * * ?");
+    cronExpresions.add("0/6 * * * * ?");
+    cronExpresions.add("0/7 * * * * ?");
+    for (String cron: cronExpresions) {
+      CronTrigger croneTrigger = new CronTrigger(cron, TimeZone.getDefault());
+      newTaskRegistrar.addCronTask(new CronTask(() -> scheduleCron(cron), croneTrigger));
     }
+  }
 
-    /**
-     * Creates new task by cron expressions from DB.
-     */
-
-    @Override
-    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
-        newTaskRegistrar = taskRegistrar;
-        newTaskRegistrar.setScheduler(poolScheduler());
-//        cronExpresions.add(schedulerRepository.findAll());
-
-        // this is only for testing, in final version cronExpresions list will come from db
-        cronExpresions.add("* * * * * ?");
-        cronExpresions.add("0/2 * * * * ?");
-        cronExpresions.add("0/3 * * * * ?");
-        cronExpresions.add("0/4 * * * * ?");
-        cronExpresions.add("0/5 * * * * ?");
-        cronExpresions.add("0/6 * * * * ?");
-        cronExpresions.add("0/7 * * * * ?");
-        for (String cron: cronExpresions) {
-                CronTrigger croneTrigger = new CronTrigger(cron, TimeZone.getDefault());
-                newTaskRegistrar.addCronTask(new CronTask(() -> scheduleCron(cron), croneTrigger));
-        }
+  /**
+   * Place for init task.
+   */
+  public void scheduleCron(String cron) {
+    LOGGER.info("Next execution time of this taken from cron expression -> {}", cron);
+    System.out.println("Next execution time of this taken from cron expression -> " + cron);
+    if (cron.equals("0/7 * * * * ?")) {
+      //just for testing
+      cancelAllTask();
     }
+  }
 
-    /**
-     * Place for init task
-     */
-    public void scheduleCron(String cron) {
-        LOGGER.info("Next execution time of this taken from cron expression -> {}", cron);
-        System.out.println("Next execution time of this taken from cron expression -> " + cron);
-        if(cron == "0/7 * * * * ?"){  //just for testing
-            cancelAllTask();
-        }
-    }
-
-    /**
-     * Delete all existing Task.
-     */
-    public void cancelAllTask() {
-        newTaskRegistrar.destroy();
-        System.out.println("Cancel all tasks");
-    }
+  /**
+   * Delete all existing Task.
+   */
+  public void cancelAllTask() {
+    newTaskRegistrar.destroy();
+    System.out.println("Cancel all tasks");
+  }
 
 }
