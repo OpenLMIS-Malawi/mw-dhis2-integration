542f78019875e9921aab535a6775af83aed19074
jakubjakubowskisoldevelo <jakubjakubowski@soldevelo.com>
2019-09-30 11:53:21 +0200

    MW4-57 - fixed checkstyle:

--- src/main/java/org/openlmis/integration/dhis2/repository/IntegrationRepository.java
+++ src/main/java/org/openlmis/integration/dhis2/repository/IntegrationRepository.java
@@ -22,7 +22,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 
 @JaversSpringDataAuditable
 public interface IntegrationRepository
